aca6d4953fc1935cf3517bc34c03d3ada81375d6
abuffa <andrea.buffa@exprivia.it>
2017-01-16 18:25:09 +0100

    switched to 64 bits pointers..

--- staticMemManager.c
+++ staticMemManager.c
@@ -4,8 +4,9 @@
 #include "staticMemManager.h"
 
 #define BLK_SIZE 1024
-#define GET_NUM_BLOCKS(numBytes) 32 * numBytes / (1028 * 32 + 1);
-#define GET_NUM_WORDS(numBlocks) (numBlocks % 32 == 0) ? numBlocks / 32 : numBlocks / 32 + 1;
+//#define GET_NUM_BLOCKS(numBytes) 32 * numBytes / (1032 * 32 + 1);
+#define GET_NUM_BLOCKS(numBytes) 64 * (numBytes - 8) / (1032 * 64 + 1);
+#define GET_NUM_WORDS(numBlocks) (numBlocks % 64 == 0) ? numBlocks / 64 : numBlocks / 64 + 1;
 #define MIN(X, Y) (((X) < (Y)) ? (X) : (Y))
 
 uint32_t memInit(void * const handle, const uint32_t sizeInByte) {
@@ -16,32 +17,30 @@ uint32_t memInit(void * const handle, const uint32_t sizeInByte) {
 	if  ((sizeInByte % 1024 != 0) || (sizeInByte <= BLK_SIZE))
 		return 2;
 
-	uint32_t* pHandle = (uint32_t*)handle;
+	uint64_t* pHandle = (uint64_t*)handle;
 	*pHandle = sizeInByte;
-	// 1024x + 4x + x/32 = sizeInByte
+	// 1024x + 8x + x/64 + 8 = sizeInByte
 	const uint32_t numBlocks = GET_NUM_BLOCKS(sizeInByte);
-
 	// the number of bits required for storing the status of a mem block
-	const uint32_t numWords = (numBlocks % 32 == 0) ? numBlocks / 32 : \
-		numBlocks / 32 + 1;
+	const uint32_t numWords = GET_NUM_WORDS(numBlocks);
 	uint32_t idx = 0, remBits = numBlocks;
 
 	do {
 		idx++;
 		if (idx == numWords) {
 			*(++pHandle) = ((1 << remBits) - 1);
+			*pHandle = (*pHandle == 0) ? 0xFFFFFFFFFFFFFFFF : *pHandle;
 		} else {
-			*(++pHandle) = 0xFFFFFFFF;
-			remBits -= 32;
+			*(++pHandle) = 0xFFFFFFFFFFFFFFFF;
+			remBits -= 64;
 		}
 	} while (idx < numWords);
 
-	++pHandle;
-
-	uint32_t* pFirstBlock = ((uint32_t*)handle) + sizeInByte / 4 - \
-			(numBlocks * 1024 / 4);
-	for (uint32_t blockIt = 0; blockIt < numBlocks; blockIt++) {
-		*((uint32_t**)(pHandle + blockIt)) = pFirstBlock + (1024 * blockIt / 4);
+	uint64_t* pBlocks = pHandle + 1;
+	uint64_t* pFirstBlock = (uint64_t*)handle + \
+		sizeInByte / 8 - (numBlocks * 1024 / 8);
+	for (uint64_t blockIt = 0; blockIt < numBlocks; blockIt++) {
+		*(pBlocks + blockIt) = (uint64_t)(pFirstBlock + (1024 * blockIt / 8));
 		//printf("0x%x\n", (uint32_t)(pFirstBlock + (1024 * blockIt / 4)));
 	}
 
@@ -51,19 +50,19 @@ uint32_t memInit(void * const handle, const uint32_t sizeInByte) {
 uint32_t memAlloc(void * const handle, const uint32_t size, void * ptr) {
 	if (!handle)
 		return 1;
-	uint32_t* pHandle = (uint32_t*)handle;
-	uint32_t* pBitmap = (uint32_t*)handle + 1;
+	uint64_t* pHandle = (uint64_t*)handle;
+	uint64_t* pBitmap = (uint64_t*)handle + 1;
 	uint32_t totBlocks = GET_NUM_BLOCKS(*pHandle);
 	if (size == 0 || size > totBlocks) {
 		return 3;
 	}
 	// if size is 3, sizeBitmap is 111
-	uint32_t sizeBitmap = ((1 << size) - 1);
+	uint64_t sizeBitmap = ((1 << size) - 1);
 	// look for the first pattern corresponding to sizeBtimap
 	uint32_t wordIt = 0, offset = 0, found = 0;
 	uint32_t bitmapLen = GET_NUM_WORDS(totBlocks);
 	while (wordIt < bitmapLen) {
-		while (size + offset <= MIN(32, totBlocks)) {
+		while (size + offset <= MIN(64, totBlocks)) {
 			//printf("BITM %x\n", (*(pBitmap + wordIt)));
 			//printf("PATT %x\n", sizeBitmap);
 			if ((*(pBitmap + wordIt) & sizeBitmap) == sizeBitmap) {
@@ -78,37 +77,39 @@ uint32_t memAlloc(void * const handle, const uint32_t size, void * ptr) {
 			break;
 		wordIt++;
 	}
-	//printf("FOUND %d, word %d, offset %d\n", found, wordIt, offset);
+
 	if (!found)
 		return 5;
+	//printf("FOUND %d, word %d, offset %d\n", found, wordIt, offset);
 
 	// update the pointers area.
-	uint32_t* pBlocks = pHandle + 1 + bitmapLen;
+	uint64_t* pBlocks = pHandle + 1 + bitmapLen;
 	uint32_t blockIt = 0;
-	uint32_t blockIndex = wordIt * 32 + offset; //0..n-1
-	uint32_t* pNew = pHandle + (*pHandle / 4) - \
-		(totBlocks * 1024 / 4) + blockIndex * 1024 / 4;
+	uint32_t blockIndex = wordIt * 64 + offset; //0..n-1
+	uint64_t* pNew = pHandle + (*pHandle / 8) - \
+		(totBlocks * 1024 / 8) + blockIndex * 1024 / 8;
 
 	while (blockIt < size) {
-		*((uint32_t**)(pBlocks + wordIt * 32 + offset + blockIt)) = pNew;
+		*((pBlocks + wordIt * 64 + offset + blockIt)) = (uint64_t)pNew ;
 		blockIt++;
 	}
 	// return the pointer to the allocated chunk
-	*((uint32_t**)ptr) = pNew;
+	*((uint64_t**)ptr) = pNew;
 	return 0;
 }
 
 uint32_t memFree(void * const handle, const void* ptr) {
-	uint32_t* pHandle = (uint32_t*)handle;
-	uint32_t* pBitmap = pHandle + 1;
+	uint64_t* pHandle = (uint64_t*)handle;
+	uint64_t* pBitmap = pHandle + 1;
 	uint32_t totBlocks = GET_NUM_BLOCKS((*pHandle));
 	uint32_t bitmapSize = GET_NUM_WORDS(totBlocks);
-	uint32_t* pBlocks = pHandle + 1 + bitmapSize;
+	uint64_t* pBlocks = (uint64_t*)(pHandle + 1 + bitmapSize);
 	uint32_t idx = 0, found = 0, chunkStart = 0, chunkLen = 0;
 	while (idx < totBlocks) {
-		if ((*(pBlocks + idx)) == (*(uint32_t*)ptr)) {
-			*(uint32_t**)(pBlocks + idx) = pHandle + (*pHandle / 4) - \
-				(totBlocks * 1024 / 4) + (idx * 1024 / 4);
+		if ((*(pBlocks + idx)) == (*(uint64_t*)ptr)) {
+			//printf("PT %x, idx %d\n", *(pBlocks + idx), idx);
+			*(pBlocks + idx) = (uint64_t)(pHandle + (*pHandle / 8) - \
+				(totBlocks * 1024 / 8) + (idx * 1024 / 8));
 			if (!found) {
 				chunkStart = idx;
 			}
@@ -121,34 +122,34 @@ uint32_t memFree(void * const handle, const void* ptr) {
 	//printf("FOUND chunkLen %d, start %d\n", chunkLen, chunkStart);
 	if (!found)
 		return 4;
-	uint32_t sizeBitmap = ((1 << chunkLen) - 1);
+	uint64_t sizeBitmap = ((1 << chunkLen) - 1);
 	//printf("FOUND sizeBitmap %x\n", sizeBitmap);
 	// get the word containing the bitmap to be updated
-	*(pBitmap + (chunkStart / 32)) |= sizeBitmap << (chunkStart % 32);
+	*(pBitmap + (chunkStart / 64)) |= sizeBitmap << (chunkStart % 64);
 	return 0;
 }
 
 uint32_t memDump(void* const handle, uint32_t verb) {
-	uint32_t* pHandle = (uint32_t*)handle;
+	uint64_t* pHandle = (uint64_t*)handle;
 	uint32_t numBlocks = GET_NUM_BLOCKS((*pHandle));
 	uint32_t numWords = GET_NUM_WORDS(numBlocks);
-	printf("memDump Total buffer size: 0x%X\n", *pHandle);
+	printf("memDump Total buffer size: 0x%lX\n", *pHandle);
 	printf("memDump Total num of 1024B blocks: %d\n", numBlocks);
-	printf("memDump for storing the alloc bitmap %d\n", numWords);
+	printf("memDump Num words for the alloc bitmap %d\n", numWords);
 
 	if (verb == 1) {
 
 		printf("memDump Allocation Bitmap: ");
 		for (uint32_t i = 0; i < numWords; i++) {
-			printf(" 0x%X ", (uint32_t)*(pHandle + 1 + i));
+			printf(" 0x%lX ", *(pHandle + 1 + i));
 		}
 		printf("\n");
 
 		printf("memDump Blocks pointers area: \n");
-		uint32_t* pBlock = pHandle + 1 + numWords;
+		uint64_t* pBlock = pHandle + 1 + numWords;
 		for (uint32_t blockIdx = 0; blockIdx < numBlocks; blockIdx++) {
-			printf("memDump block %d pointer 0x%X\n", blockIdx + 1,
-				(uint32_t)*(pBlock + blockIdx));
+			printf("memDump block %4d pointer 0x%lX\n", blockIdx + 1,
+				*(pBlock + blockIdx));
 		}
 		printf("\n");
 	}
--- test.c
+++ test.c
@@ -2,7 +2,8 @@
 #include "staticMemManager.h"
 
 #define BUFFER_SIZE_1 10240
-#define BUFFER_SIZE_2 0x2000000
+//#define BUFFER_SIZE_2 0x2000000
+#define BUFFER_SIZE_2 0x10400
 char smallBuffer[BUFFER_SIZE_1];
 char bigBuffer[BUFFER_SIZE_2];
 
@@ -15,7 +16,7 @@ int test(void* pBuf, uint32_t size) {
 	printf("Buffer pointer 0x%X\n", pBuf);
 	memInit((void*)pBuf, size);
 
-	memDump((void*)pBuf, 0);
+	memDump((void*)pBuf, 1);
 
 	//Check the first word is equal to the lenght BUFFER_SIZE
 	uint32_t* tmp = (uint32_t*)pBuf;
@@ -40,6 +41,7 @@ int test(void* pBuf, uint32_t size) {
 		line = __LINE__;
 		goto error;
 	}
+
 	//fill p1
 	for(uint32_t i = 0; i < (1 * 1024 / 4); i++) {
 		*(((uint32_t*)p1) + i) = i;
@@ -57,16 +59,18 @@ int test(void* pBuf, uint32_t size) {
 		line = __LINE__;
 		goto error;
 	}
+
 	// fill p2
 	for(uint32_t i = 0; i < (4 * 1024 / 4); i++) {
 		*(p2 + i) = i;
-		//printf("0x%X, val %d ", p2 + i, *(p2 + i));
+		//printf("0x%X, val %d \n", p2 + i, *(p2 + i));
 
 	}
+
 	// check p1 data
 	for(uint32_t i = 0; i < (1 * 1024 / 4); i++) {
 		if (*((uint32_t*)p1 + i) != i) {
-			printf("FAIL idx %d, val %d", i, *((uint32_t*)p1 + i));
+			printf("FAIL idx %d, val %d \n", i, *((uint32_t*)p1 + i));
 			line = __LINE__;
 			goto error;
 		}
@@ -77,6 +81,7 @@ int test(void* pBuf, uint32_t size) {
 		line = __LINE__;
 		goto error;
 	}
+
 	// fill p3
 	for(uint32_t i = 0; i < (4 * 1024 / 4); i++) {
 		*(p3 + i) = i;
@@ -92,31 +97,81 @@ int test(void* pBuf, uint32_t size) {
 		}
 	}
 
-	printf("Test MemFree 1. Deleting 0x%X\n", p3);
+	printf("Test MemFree p3. Deleting 0x%X\n", p3);
 	if (0 != memFree((void*)pBuf, &p3)) {
 		line = __LINE__;
 		goto error;
 	}
 
-	printf("Test MemFree 2. Deleting 0x%X\n", p1);
+	printf("Test MemFree p1. Deleting 0x%X\n", p1);
 	if (0 != memFree((void*)pBuf, &p1)) {
 		line = __LINE__;
 		goto error;
 	}
-	memDump((void*)pBuf, 0);
+
+	printf("Test MemFree p2. Deleting 0x%X\n", p2);
+	if (0 != memFree((void*)pBuf, &p2)) {
+		line = __LINE__;
+		goto error;
+	}
+
+	//memDump((void*)pBuf, 1);
 	printf("SUCCESS!!!!!!!!\n");
 	return 0;
 
 error:
-	memDump((void*)pBuf, 0);
+	memDump((void*)pBuf, 1);
 	printf("Test at line %d FAILED\n", line);
-	return 0;
+	return 1;
 }
 
+test2(void* pBuf, uint32_t size) {
+	uint32_t* p1 = 0;
+	uint32_t* p2 = 0;
+	uint32_t* p3 = 0;
+	uint32_t* p4 = 0;
+	uint32_t* p5 = 0;
+	uint32_t line = 0;
+
+	memInit((void*)pBuf, size);
+
+	printf("Test memAlloc 40 blocks 1\n");
+	if (0 != memAlloc((void*)pBuf, 40, &p1)) {
+		line = __LINE__;
+		goto error;
+	}
+
+	printf("Test memAlloc 40 blocks 2\n");
+	if (0 != memAlloc((void*)pBuf, 40, &p2)) {
+		line = __LINE__;
+		goto error;
+	}
+
+	memDump((void*)pBuf, 1);
+
+	return 0;
+error:
+	memDump((void*)pBuf, 0);
+	printf("Test at line %d FAILED\n", line);
+	return 1;
+}
 
 int main(void) {
-	test(smallBuffer, BUFFER_SIZE_1);
+	if (0 != test(smallBuffer, BUFFER_SIZE_1)) {
+		goto error;
+	}
+
 	printf("Use another buffer *********\n");
-	test(bigBuffer, BUFFER_SIZE_2);
+	if (0 != test(bigBuffer, BUFFER_SIZE_2)) {
+		goto error;
+	}
+	printf("More extra test    *********\n");
+	if (0 != test2(bigBuffer, BUFFER_SIZE_2)) {
+		goto error;
+	}
+
+	printf("SUCCESS!!!!!!!!\n");
 	return 0;
+error:
+	return 1;
 }
